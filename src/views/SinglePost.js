de5e81e66ad473a326fc05959bcca807df09449e
Dusty McCord <Dustymccord@gmail.com>
2020-07-06 22:39:35 -0700

    fix build error and bug.

--- src/views/SinglePost.js
+++ src/views/SinglePost.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
-import { ChevronLeft } from 'react-feather'
+// import { ChevronLeft } from 'react-feather'
 
 import Content from '../components/Content'
 import BackgroundImage from '../components/BackgroundImage'
