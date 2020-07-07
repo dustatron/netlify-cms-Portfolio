aa20f7f89fb659e32250a0fa50ffe456c0e69197
Dusty McCord <Dustymccord@gmail.com>
2020-07-06 22:23:33 -0700

    update singlePost.

--- src/views/SinglePost.js
+++ src/views/SinglePost.js
@@ -20,6 +20,7 @@ export default ({ fields, nextPostURL, prevPostURL }) => {
       )}
 
       <div className="container skinny">
+        hello
         {/* <Link className="SinglePost--BackButton" to="/blog/">
           <ChevronLeft /> BACK
         </Link> */}
